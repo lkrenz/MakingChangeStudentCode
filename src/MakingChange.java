8befe9527d35e67b6a838e55b19ff1a39ffc1eab
lkrenz <liamkrenz100@gmail.com>
2025-01-29 11:28:39 -0800

    started coding

--- src/MakingChange.java
+++ src/MakingChange.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * The class Making Change solves a classic problem:
  * given a set of coins, how many ways can you make change for a target amount?
@@ -12,6 +14,36 @@ public class MakingChange {
      *  for any given total with any given set of coins.
      */
     public static long countWays(int target, int[] coins) {
-        return 0;
+        long[] paths = new long[target + 1];
+        coins = sortArray(coins);
+        recurseSolution(paths, coins, target, coins.length -1);
+        return paths[target];
+    }
+
+    public static int[] sortArray(int[] coins) {
+        Arrays.sort(coins);
+        return coins;
+    }
+
+    public static int recurseSolution(long[] paths, int[] coins, int currVal, int lowestRemoved) {
+        if (currVal == 0) {
+            return 1;
+        }
+        if (currVal < 0) {
+            return 0;
+        }
+
+        long numPaths = 0;
+
+        for (int i = lowestRemoved; i > 0; i--) {
+            int newTotal = currVal - coins[i];
+            if (paths[newTotal] != 0) {
+                numPaths += paths[newTotal];
+            }
+            else {
+                paths[newTotal] = recurseSolution(paths, coins, newTotal, i);
+            }
+            numPaths += paths[newTotal];
+        }
     }
 }
